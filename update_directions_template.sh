9c7a4d5c860bf2fb53363356f3f7306323bcc9d7
Artem Svetlov <trolleway@yandex.ru>
2019-04-25 16:25:48 +0300

    Update update_directions_template.sh

--- update_directions_template.sh
+++ update_directions_template.sh
@@ -5,5 +5,5 @@ mapillary_tools process --advanced --import_path "$PWD" --user_name $USERNAME --
 
 for file in "$PWD"
 do
-  "c:\gis\pano_heading\exiftool.exe" -overwrite_original -quiet -ProjectionType="equirectangular" -UsePanoramaViewer="True" -"PoseHeadingDegrees<$exif:GPSImgDirection" -"CroppedAreaImageWidthPixels<$ImageWidth" -"CroppedAreaImageHeightPixels<$ImageHeight" -"FullPanoWidthPixels<$ImageWidth" -"FullPanoHeightPixels<$ImageHeight" -CroppedAreaLeftPixels="0" -CroppedAreaTopPixels="0" $file
+  exiftool -overwrite_original -quiet -ProjectionType="equirectangular" -UsePanoramaViewer="True" -"PoseHeadingDegrees<$exif:GPSImgDirection" -"CroppedAreaImageWidthPixels<$ImageWidth" -"CroppedAreaImageHeightPixels<$ImageHeight" -"FullPanoWidthPixels<$ImageWidth" -"FullPanoHeightPixels<$ImageHeight" -CroppedAreaLeftPixels="0" -CroppedAreaTopPixels="0" $file
 done
