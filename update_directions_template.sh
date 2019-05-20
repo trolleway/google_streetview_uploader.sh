3797cb81c3d8304f3aaeb3b23ec6e6c550d32df4
Artem Svetlov <trolleway@yandex.ru>
2019-05-21 01:42:04 +0300

    Update update_directions_template.sh

--- update_directions_template.sh
+++ update_directions_template.sh
@@ -1,6 +1,6 @@
 USERNAME="trolleway"
 
-mapillary_tools process --advanced --import_path "$PWD" --user_name $USERNAME --cutoff_distance 100  --interpolate_directions --offset_angle 180 --rerun --overwrite_EXIF_direction_tag
+mapillary_tools process --advanced --import_path "$PWD" --user_name $USERNAME --cutoff_distance 100 --cutoff_time 60 --interpolate_directions --offset_angle 180 --rerun --overwrite_EXIF_direction_tag
 
 
 for file in "$PWD"
