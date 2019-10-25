6d8469e5e26fa71ac7e4ef6bc5a4cd8a1ada40b5
Artem Svetlov <trolleway@yandex.ru>
2019-10-25 19:12:59 +0300

    Update update_directions_template.sh

--- update_directions_template.sh
+++ update_directions_template.sh
@@ -10,6 +10,7 @@ mapillary_tools process --advanced --import_path "$PWD" --user_name $USERNAME --
 for file in "$PWD"
 do
   exiftool -overwrite_original -quiet -ProjectionType="equirectangular" -UsePanoramaViewer="True" -"PoseHeadingDegrees<$exif:GPSImgDirection" -"CroppedAreaImageWidthPixels<$ImageWidth" -"CroppedAreaImageHeightPixels<$ImageHeight" -"FullPanoWidthPixels<$ImageWidth" -"FullPanoHeightPixels<$ImageHeight" -CroppedAreaLeftPixels="0" -CroppedAreaTopPixels="0" "$file" mapillary_tools process --advanced --import_path "$PWD" --user_name $USERNAME --cutoff_distance 100 --cutoff_time 60 --interpolate_directions --offset_angle $ANGLE --rerun --overwrite_EXIF_direction_tag 2> /dev/null
+  printf "."
 done
 
 mapillary_tools upload --import_path "$PWD" --skip_subfolders --number_threads 5 --max_attempts 10 --advanced
